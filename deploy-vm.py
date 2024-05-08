a5218eee1df13ecbf5ac5cf122b6c8ec9faf3fe8
Saad-Eddine-Omary <saad.eddine.omary12@gmail.com>
2024-05-08 12:43:56 +0200

    edited deploy vm

--- deploy-vm.py
+++ deploy-vm.py
@@ -1,26 +1,13 @@
 import sys
-from proxmoxer import ProxmoxAPI
-import urllib3
-
-urllib3.disable_warnings()
+import os
+from proxmox_api import create_proxmox_api
 
 # Proxmox server details
-proxmox_host = '192.168.1.169'
-username = 'root@pam'
-node = 'proxmox'
-api_token = '574a4eb4-7f2f-4845-8e45-9506fc263a0f'
-vmid = '9000'
-newvmid = '1100'
-newvmname = 'PyDeployment'
+#vmid = '9000'
+#newvmid = '1100'
+#newvmname = 'PyDeployment'
 
-def create_proxmox_api():
-    try:
-        return ProxmoxAPI(proxmox_host, user=username, token_name='testDeploy', token_value=api_token, verify_ssl=False)
-    except Exception as e:
-        print(f"Error creating Proxmox API object: {e}")
-        sys.exit(1)
-
-def clone_vm(proxmox):
+def clone_vm(proxmox, vmid, newvmid, newvmname, node):
     try:
         proxmox.nodes(node).qemu(vmid).clone.create(
             newid=newvmid,
@@ -31,14 +18,19 @@ def clone_vm(proxmox):
         print(f"Failed to clone VM {vmid}. Error: {e}")
 
 def main():
-    if len(sys.argv) < 2:
-        print("Usage: python script_name.py <api_token>")
+    if len(sys.argv) < 4:
+        print("Usage: python script_name.py <api_token> <vmid> <newvmid> <newvmname>")
         sys.exit(1)
 
     api_token = sys.argv[1]
+    vmid = sys.argv[2] 
+    newvmid = sys.argv[3] 
+    newvmname = sys.argv[4] 
 
-    proxmox = create_proxmox_api()
-    clone_vm(proxmox)
+    node = os.getenv("NODE")
+
+    proxmox = create_proxmox_api(api_token)
+    clone_vm(proxmox, vmid, newvmid, newvmname, node)
 
 if __name__ == "__main__":
     main()
