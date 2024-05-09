8e42fa08202cff608ed01de3572840b4187002fd
Saad-Eddine-Omary <saad.eddine.omary12@gmail.com>
2024-05-09 15:17:29 +0200

    1 function to restore them all

--- installer.py
+++ installer.py
@@ -1,48 +1,36 @@
 import sys
 import os
-import json
-from proxmox_api import create_proxmox_api
-from restoreFuncs import *
+from restoreFuncs import restore_backups
 from runOnProxmox import runOnProxmox
 
 def main():
+    if len(sys.argv) < 3:
+        print("Usage: python installer.py <api_token> <backup_pairs.json> <proxmox_ssh_password>")
+        sys.exit(1)
+#=========================GET BAKCUP FILES FROM SFTP SERVER INTO THE PROXMOX SERVER=========================
+
     server = {
-        "hostname" : os.getenv("PROXMOX_HOST"),
-        "username" : "root",
-        "password" : sys.argv[3]
+        "hostname" : os.getenv("proxmoxhost"),
+        "username" : os.getenv("proxmoxuser"),
+        "password" : sys.argv[3],
+        "command" : os.getenv("command")
     }
-    commandToRun = "./getBkps.sh"
-    runOnProxmox(server["hostname"], server["username"], server["password"], commandToRun)
-#    if len(sys.argv) < 3:
-#        print("Usage: python restore-mass-backups-lxc.py <api_token> backup_pairs.json")
-#        sys.exit(1)
-#
-#    api_token = sys.argv[1]
-#    json_file = sys.argv[2]
-#
-#    node = os.getenv("NODE")
-#
-#    proxmox = create_proxmox_api(api_token)
-#
-#    if proxmox:
-#        try:
-#            with open(json_file, 'r') as f:
-#                backup_pairs = json.load(f)
-#            
-#            for backup_file, vmid in backup_pairs.items():
-#                if "lxc" in backup_file:
-#                    restore_lxc_backup(proxmox, backup_file, node, vmid)
-#                elif "qemu" in backup_file:
-#                    restore_qemu_backup(proxmox, backup_file, node, vmid)
-#                else:
-#                    print("Error unknown backup file")
-#                    return 1
-#        except FileNotFoundError:
-#            print(f"File not found: {json_file}")
-#        except json.JSONDecodeError as e:
-#            print(f"Error parsing JSON in {json_file}: {e}")
-#        except ValueError as e:
-#            print(f"Invalid JSON format in {json_file}: {e}")
+    
+    runOnProxmox(server["hostname"], server["username"], server["password"], server["command"])
+
+#=========================RESTORE BACKUP FILES=========================
+
+    api_token = sys.argv[1]
+    json_file = sys.argv[2]
+    node = os.getenv("NODE")
+
+    restore_backups(api_token, json_file, node)
+
+#=========================DEPLOY MACHINES=========================
+
+
+
+
 
 if __name__ == "__main__":
     main()
--- restoreFuncs.py
+++ restoreFuncs.py
@@ -1,3 +1,6 @@
+import json
+from proxmox_api import create_proxmox_api
+
 def restore_lxc_backup(proxmox, backup_file, node, vmid):
     try:
         proxmox.nodes(node).lxc.create(
@@ -20,3 +23,26 @@ def restore_qemu_backup(proxmox, backup_file, node, vmid):
         print(f"Backup {backup_file} restored successfully.")
     except Exception as e:
         print(f"Failed to restore backup {backup_file}. Error: {e}")
+
+def restore_backups(api_token,json_file, node):
+    proxmox = create_proxmox_api(api_token)
+
+    if proxmox:
+        try:
+            with open(json_file, 'r') as f:
+                backup_pairs = json.load(f)
+            
+            for backup_file, vmid in backup_pairs.items():
+                if "lxc" in backup_file:
+                    restore_lxc_backup(proxmox, backup_file, node, vmid)
+                elif "qemu" in backup_file:
+                    restore_qemu_backup(proxmox, backup_file, node, vmid)
+                else:
+                    print("Error unknown backup file")
+                    return 1
+        except FileNotFoundError:
+            print(f"File not found: {json_file}")
+        except json.JSONDecodeError as e:
+            print(f"Error parsing JSON in {json_file}: {e}")
+        except ValueError as e:
+            print(f"Invalid JSON format in {json_file}: {e}")
